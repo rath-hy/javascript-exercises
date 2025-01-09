c9559ae2381ab8aa1242b288cf6b95b1c906943e
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-09 09:50:00 -0800

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reversed_str = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed_str += str.at(i);
+    }
+    return reversed_str;
 };
 
 // Do not edit below this line
