056ab5aa064008008260fa511ccee721d3514a8a
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-09 10:11:50 -0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+
+    for (let itemNumber = 1; itemNumber < arguments.length; itemNumber++) {
+        arr = removeOneItemFromArray(arr, arguments[itemNumber]);
+    }
+
+    return arr;
 };
+
+const removeOneItemFromArray = function(arr, itemToRemove) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === itemToRemove) {
+            arr = arr.toSpliced(i, 1);
+            i = 0;
+        }
+    }
+
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
