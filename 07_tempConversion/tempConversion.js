f03dbefd5bea300ca1bc5a789eea4f1ac272302c
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-09 10:57:47 -0800

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  result = ((5 / 9) * (fahrenheit - 32));
+
+  if (!Number.isInteger(result))
+    return Number(result.toFixed(1));
+
+  return Number(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  result = (((9 / 5) * (celsius)) + 32);
+
+  if (!Number.isInteger(result))
+    return Number(result.toFixed(1));
+
+  return Number(result);
 };
 
 // Do not edit below this line
