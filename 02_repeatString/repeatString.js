3f4b8e76c2aefadfc8f13b3eff0047848b757eac
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-09 09:43:33 -0800

    Complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, numberOfRepetitions) {
+    new_str = str;
 
+    if (numberOfRepetitions === 0)
+        return '';
+
+    else if (numberOfRepetitions < 0)
+        return 'ERROR';
+
+    for (let i = 1; i < numberOfRepetitions; i++) {
+        new_str += str;
+    }
+    return new_str;
 };
 
 // Do not edit below this line
