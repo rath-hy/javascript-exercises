691ed769fb1e9d6d4bd7eb1c477c9203bb81f7bb
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-09 10:44:24 -0800

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 != 0 ? false : year % 100 != 0 ? true : year % 400 != 0 ? false : true;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
